519c7140a82c376b61d89adf09fc32e03bf32139
Fanzil <fanzil.fattakhov@quickex.io>
2020-01-04 22:14:29 +0300

    Для heroku

--- src/main/java/com/javamaster/controller/HomeServlet.java
+++ src/main/java/com/javamaster/controller/HomeServlet.java
@@ -7,20 +7,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.javamaster.dao.ArticleDao;
-import com.javamaster.dao.impl.ArticleDaoImpl;
-import com.javamaster.entity.Article;
-import com.javamaster.entity.Category;
 
 /**
  * Servlet implementation class HomeServlet
  */
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	private ArticleDao articleDao = new ArticleDaoImpl();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -36,9 +28,7 @@ public class HomeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String path = request.getServletPath();
 		if (path.equals("/")){
-			List<Article> articles = articleDao.getAllArticles();
 			request.setAttribute("title", "Title of our application");
-			request.setAttribute("articles", articles);
 			request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
 		}
 		
